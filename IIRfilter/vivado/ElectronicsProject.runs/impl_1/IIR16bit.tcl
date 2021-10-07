4da822f9e127a4a1669ac1f79610ea09ed731b4e
Leonardo Poggiani <leonardo.poggiani@gmail.com>
2021-10-07 08:12:39 +0200

    doc images

--- IIRfilter/vivado/ElectronicsProject.runs/impl_1/IIR16bit.tcl
+++ IIRfilter/vivado/ElectronicsProject.runs/impl_1/IIR16bit.tcl
@@ -60,24 +60,17 @@ proc step_failed { step } {
   close $ch
 }
 
-set_msg_config -id {Synth 8-256} -limit 10000
-set_msg_config -id {Synth 8-638} -limit 10000
 
 start_step init_design
 set ACTIVE_STEP init_design
 set rc [catch {
   create_msg_db init_design.pb
-  set_param synth.incrementalSynthesisCache C:/Users/leona/AppData/Roaming/Xilinx/Vivado/.Xil/Vivado-2188-LEO-DEV/incrSyn
-  create_project -in_memory -part xc7z010clg400-1
-  set_property design_mode GateLvl [current_fileset]
-  set_param project.singleFileAddWarning.threshold 0
+  reset_param project.defaultXPMLibraries 
+  open_checkpoint C:/Users/leona/Dropbox/PC/Documents/GitHub/ElectronicsProject/IIRfilter/vivado/ElectronicsProject.runs/impl_1/IIR16bit.dcp
   set_property webtalk.parent_dir C:/Users/leona/Dropbox/PC/Documents/GitHub/ElectronicsProject/IIRfilter/vivado/ElectronicsProject.cache/wt [current_project]
   set_property parent.project_path C:/Users/leona/Dropbox/PC/Documents/GitHub/ElectronicsProject/IIRfilter/vivado/ElectronicsProject.xpr [current_project]
   set_property ip_output_repo C:/Users/leona/Dropbox/PC/Documents/GitHub/ElectronicsProject/IIRfilter/vivado/ElectronicsProject.cache/ip [current_project]
   set_property ip_cache_permissions {read write} [current_project]
-  add_files -quiet C:/Users/leona/Dropbox/PC/Documents/GitHub/ElectronicsProject/IIRfilter/vivado/ElectronicsProject.runs/synth_1/IIR16bit.dcp
-  read_xdc C:/Users/leona/Dropbox/PC/Documents/GitHub/ElectronicsProject/IIRfilter/vivado/ElectronicsProject.srcs/constrs_1/new/clock_constrain.xdc
-  link_design -top IIR16bit -part xc7z010clg400-1
   close_msg_db -file init_design.pb
 } RESULT]
 if {$rc} {
